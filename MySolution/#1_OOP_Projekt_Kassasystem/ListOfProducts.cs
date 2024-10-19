7d8b66d2e9da0ffb08574059f64400d378908176
Kamyar Shateri Kashi <kamyarshateri@gmail.com>
2024-10-19 16:15:06 +0200

    created productmanager, listofproducts, added some error handling

---
+++ MySolution/#1_OOP_Projekt_Kassasystem/ListOfProducts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_OOP_Projekt_Kassasystem
+{
+    public class ListOfProducts
+    {
+        public List<Product> Products {  get; set; } = new List<Product>();
+
+
+        public void AddProduct(Product product)
+        {
+            Products.Add(product);
+        }
+    }
+}
